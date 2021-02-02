ce4a5bd4341e275a80947158a7b7268e1e5d2c6d
Kyu-u <melanchthonbutarbutar@gmail.com>
2021-02-02 20:41:05 +0700

    first commit

---
+++ public/script.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import VueCarousel from 'vue-carousel';
+Vue.use(VueCarousel);
+
+import { Carousel, Slide } from 'vue-carousel';
+export default {
+
+  components: {
+    Carousel,
+    Slide
+  }
+
+};
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        body: ['Poppins']
+      },
+
+      colors: {
+        navselect : '#222534',
+        nav : '#888AA2',
+        grid : '#F1F5FA',
+      },
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
